009c723d9a2f4b281876bfb21dfba6f4a62c5da0
Ryan Ritchie <ryan@ryan-ritchie.co.uk>
2022-10-24 18:56:22 +0100

    models

--- app/models.py
+++ app/models.py
@@ -1,107 +1,73 @@
-from app import db
+from app import db, login
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-from sqlalchemy.orm import backref
+from sqlalchemy.orm import backref, validates
 import uuid
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def generate_uuid():
     return str(uuid.uuid4())
 
-class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), index=True, unique=True, default=generate_uuid)
-    name = db.Column(db.String(32), index=True)
+@login.user_loader
+def load_user(id):
+    return Profile.query.get(int(id))
+
+class Profile(db.Model, UserMixin):
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    username = db.Column(db.String(32), index=True)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(96), index=True, nullable=False)
 
     recurring = db.relationship('RecurringRecord', back_populates='profile')
     ledgers = db.relationship('Ledger', back_populates='profile')
 
     def __repr__(self):
-        return f'Profile<{self.name}>'
+        return f'<Profile: {self.username}>'
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-class RecurringRecord(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), index=True, unique=True, default=generate_uuid)
-    amount = db.Column(db.Integer)
-    name = db.Column(db.String(64), index=True)
-    description = db.Column(db.String(512))
-    recurring_dom = db.Column(db.Integer, default=1)
-    payment_method = db.Column(db.String(12), default='manual')
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
-    profile = db.relationship('Profile', back_populates='recurring')
+class DiaryEntry(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    day = db.Column(db.Date, nullable=False)
 
-    def __repr__(self):
-        return f'RecurringRecord<{self.name}>'
+class Meal(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    category = db.Column(db.String(32))
 
-class Record(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    description = db.Column(db.String(512))
-    uuid = db.Column(db.String(36), index=True, unique=True, default=generate_uuid)
-    amount = db.Column(db.Integer)
-    recurring_dom = db.Column(db.Integer, default=1)
-    payment_method = db.Column(db.String(12), default='manual')
-    paid = db.Column(db.Boolean, default=False)
+class FoodEntry(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    quantity = db.Column(db.Integer, nullable=False)
 
-    ledger_id = db.Column(db.Integer, db.ForeignKey('ledger.id'))
-    ledger = db.relationship('Ledger', back_populates='records')
+    # relationships
+    food = db.relationship('Food', back_populates='foodentry', uselist=False)
 
-    def __repr__(self):
-        return f'Record<{self.name}>'
+    meal_id = db.Column(db.String(36), db.ForeignKey('meal.id'))
+    # TODO: finish
 
-class Ledger(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), index=True, unique=True, default=generate_uuid)
-    month = db.Column(db.Date)
+    @validates('quantity')
+    def validate_quantity(self, key, amount):
+        assert amount > 0
+        return amount
+    
+class Food(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    name = db.Column(db.String(32), index=True, nullable=False)
+    barcode = db.Column(db.String(32), index=True, nullable=False)
+    brand = db.Column(db.String(32))
+    calories_kcal = db.Column(db.Integer)
+    fat = db.Column(db.Float)
+    saturated_fat = db.Column(db.Float)
+    carbohydrate = db.Column(db.Float)
+    sugar = db.Column(db.Float)
+    protein = db.Column(db.Float)
+    salt = db.Column(db.Float)
+    fibre = db.Column(db.Float)
 
-    records = db.relationship('Record', back_populates='ledger')
-    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
-    profile = db.relationship('Profile', back_populates='ledgers')
-
-    def __repr__(self):
-        return f'Ledger<{self.month}>'
-
-"""
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), index=True, unique=True, default=generate_uuid)
-    amount = db.Column(db.Integer)
-    recurring = db.Column(db.Boolean, default=False)
-    recurring_dom = db.Column(db.Integer)
-    name = db.Column(db.String(64), index=True)
-    description = db.Column(db.String(512))
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
-    end_month = db.Column(db.Date, index=True)
-    active = db.Column(db.Boolean, default=True)
-    type = db.Column(db.Integer)
-    period = db.Column(db.Float)
-    month = db.Column(db.Date)
-
-    def __repr__(self):
-        return f'Item<{self.name}>'
-
-    def _update_month(self):
-        self.month += relativedelta(months=self.period)
-
-class LedgerEntry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    description = db.Column(db.String(512))
-    amount = db.Column(db.Integer)
-    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
-    item = db.relationship("Item")
-
-    ledger_id = db.Column(db.Integer, db.ForeignKey('ledger.id'))
-
-    def __repr__(self):
-        return f'LedgerEntry<ledger: {self.ledger_id}, item: {self.item_id}>'
-
-class Ledger(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    month = db.Column(db.Date)
-    entries = db.relationship('LedgerEntry', backref='ledger', lazy='dynamic')
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
-
-    def __repr__(self):
-        return f'Ledger<{self.month}>'
-"""
+    # relationships
+    foodentry_id = db.Column(db.String(36), db.ForeignKey('foodentry.id'))
+    foodentry = db.relationship('FoodEntry', back_populates='food')
